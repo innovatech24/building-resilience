141f6a546bfb0e1d4f0681cd0b350f8e2b898bbb
Kiran Surendran <kiran_surendran@outlook.com>
2019-04-03 00:11:34 +1100

    Add project files.

---
+++ Resilience/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Resilience.Startup))]
+namespace Resilience
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
