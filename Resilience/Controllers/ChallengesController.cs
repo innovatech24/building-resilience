4d252ff4cac56a51b7db4a90f265e234208c6f77
Kiran Surendran <kiran_surendran@outlook.com>
2019-04-06 09:32:26 +1100

    Added new controllers, route setup for new pages.

---
+++ Resilience/Controllers/ChallengesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Resilience.Controllers
+{
+    public class ChallengesController : Controller
+    {
+        // GET: Challenges
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Bullying()
+        {
+            return View();
+        }
+
+        public ActionResult Abuse()
+        {
+            return View();
+        }
+
+        public ActionResult Violence()
+        {
+            return View();
+        }
+    }
+}
