06089497b6433b7732046db38324da10d52454d2
arturoramoss <arti.ramos.s@gmail.com>
2019-04-26 20:53:35 +1000

    Adding sentiment analysis logic and structure.

---
+++ Resilience/Models/SentimentPy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace Resilience.Models
+{
+    public class SentimentPy
+    {
+
+        public float getSentimentScore(string text)
+        {
+            string res = run_cmd("Scripts/py/sentiment_analysis.py", text);
+            return (float.Parse(res));
+        }
+
+        public string getSentimentMinMax(string text)
+        {
+            string res = run_cmd("Scripts/py/sentiment_analysis2.py", text);
+            return (res); //Json format string
+        }
+
+        private string run_cmd(string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = @"python.exe";
+            start.CreateNoWindow = true;
+            start.Arguments = string.Format("{0} \"{1}\"", AppDomain.CurrentDomain.BaseDirectory.Replace("\\", "/") + cmd, args);
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    //Console.Write(result);
+                    //process.WaitForExit();
+                    return result;
+                }
+            }
+        }
+    }
+}
