6d015c0dc2b9e15978618468d520c262052ddcdf
Peng Dai <pdai0001@student.monash.edu>
2019-04-28 19:59:04 +1000

    change the styling of login, register, create diary entry, choice, add mentor and mentee page.

---
+++ Resilience/Scripts/choice.js
@@ -0,0 +1,14 @@
+﻿jQuery(".button1").click(function (e) {
+    window.location = jQuery(this).find("a").attr("href");
+    return false;
+});
+
+jQuery(".button2").click(function (e) {
+    window.location = jQuery(this).find("a").attr("href");
+    return false;
+});
+
+jQuery(".button3").click(function (e) {
+    window.location = jQuery(this).find("a").attr("href");
+    return false;
+});
