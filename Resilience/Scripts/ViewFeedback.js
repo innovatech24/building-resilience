0715c57d7bfe2c86f7a51bac571e0e65dd983e8e
arturoramoss <arti.ramos.s@gmail.com>
2019-05-05 19:39:59 +1000

    Added rating stars on entry feedback for the mentee

---
+++ Resilience/Scripts/ViewFeedback.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    
+    $(".rating").rateYo({
+        precision: 0,
+        onSet: function (rating, rateYoInstance) {
+            $.ajax({
+                url: '/DiaryEntries/ViewFeedback',
+                type: 'POST',
+                data: {
+                    Id: parseInt(this.getAttribute("iid")),
+                    rate: rating
+                }
+            });
+        }
+    });
+
+});
+
