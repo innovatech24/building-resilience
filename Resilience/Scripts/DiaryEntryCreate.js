b67b45bd5b53ff2c970b4ce273e2fec3c279ccc2
arturoramoss <arti.ramos.s@gmail.com>
2019-05-09 23:34:13 +1000

    Fixed confirmation message on DiaryEntry for the mentee.

--- Resilience/Scripts/DiaryEntryCreate.js
+++ Resilience/Scripts/DiaryEntryCreate.js
@@ -1,22 +1,30 @@
 ﻿$(document).ready(function () {
 
-    /*$(".rating").rateYo({
-        precision: 0,
-        onSet: function (rating, rateYoInstance) {
-            document.getElementById("MenteeFeedback").value = rating;
+    window.onbeforeunload = function () {
+        return true;
+    };
 
-        }
-    });*/
-
+    // Create range of emotions
     var emotionsArray = ['crying', 'disappointed', 'meh', 'happy', 'smile'];
 
+    // Set div with rating
     $(".rating").emotionsRating({
 
+        // Back ground emoticon
         bgEmotion: 'happy',
+
+        // Set array of emoticons
         emotions: emotionsArray,
+
+        // Everytime the value change, the hiden object binded to the model is updated
         onUpdate: function (value) {
             document.getElementById("MenteeFeedback").value = value;
         }
     });
 
 });
+
+// Just to omit the confirmation to leave the page when the create button is pressed
+function btnSubmit() {
+    window.onbeforeunload = null;
+};
