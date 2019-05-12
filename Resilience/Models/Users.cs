8518300f3dc080ddfbe630a1366ed7fc2dcf29da
Kiran Surendran <kiran_surendran@outlook.com>
2019-05-12 19:05:45 +1000

    Fixing bug about mentee viewing all goals and also validation on profile page.

--- Resilience/Models/Users.cs
+++ Resilience/Models/Users.cs
@@ -23,7 +23,11 @@ namespace Resilience.Models
         }
     
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a first name")]               
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Please enter a last name")]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public Nullable<int> MentorId { get; set; }
