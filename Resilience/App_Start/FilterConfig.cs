7d52216b93018f06d81eb3359d659696089a80fb
Kiran Surendran <kiran_surendran@outlook.com>
2019-05-22 18:14:26 +1000

    Changes to add button.

--- Resilience/App_Start/FilterConfig.cs
+++ Resilience/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace Resilience
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            //filters.Add(new RequireHttpsAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
